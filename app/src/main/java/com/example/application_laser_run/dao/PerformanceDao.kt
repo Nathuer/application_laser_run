20e32fbc3b16ce19f23eb1e5c3dc69df5efd165b
Nathuer <alandettegabriel@gmail.com>
2025-03-07 15:17:18 +0100

    commit modification dans le PerformanceDao

--- app/src/main/java/com/example/application_laser_run/dao/PerformanceDao.kt
+++ app/src/main/java/com/example/application_laser_run/dao/PerformanceDao.kt
@@ -11,18 +11,12 @@ interface PerformanceDao {
     @Query("select * from performance")
     suspend fun getAll(): List<Performance>
 
+
     @Query("select * from performance where id = :id")
     suspend fun getById(id: Long): Performance?
 
-    @Query("SELECT * FROM performance ORDER BY id DESC LIMIT 1")
-    suspend fun getLastPerformance(): Performance?
-
-    @Query("SELECT * FROM Performance WHERE category_ref = :categoryName")
-    suspend fun getPerformancesByCategory(categoryName: String): List<Performance>
-
-    @Query("SELECT SUM(run_duration) FROM performance")
-    suspend fun getTotalRunDuration(): Long?
-
+    @Query("select * from performance order by id desc limit 1")
+    suspend fun getLastPerformance(): Performance
 
     @Insert
     suspend fun insert(vararg performance: Performance)
