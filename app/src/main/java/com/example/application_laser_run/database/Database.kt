70160b0f5e659f0cc7124a673c630d37629394aa
Nathuer <alandettegabriel@gmail.com>
2025-03-07 15:12:29 +0100

    commit modification dans la db

--- app/src/main/java/com/example/application_laser_run/database/Database.kt
+++ app/src/main/java/com/example/application_laser_run/database/Database.kt
@@ -9,7 +9,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.application_laser_run.dao.PerformanceDao
 import com.example.application_laser_run.model.Performance
 
-@Database(entities = [Performance::class], version = 2) // Version 2
+@Database(entities = [Performance::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun performanceDao(): PerformanceDao
 
@@ -17,25 +17,16 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
-        fun getInstance(context: Context): AppDatabase {
+        fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "laser_run_db"
+                    "laser_run_data"
                 )
-                    .addMigrations(MIGRATION_1_2) // Ajoute la migration ici
                     .build()
                 INSTANCE = instance
                 instance
-            }
-        }
-
-        // Migration pour ajouter le champ start_time
-        val MIGRATION_1_2 = object : Migration(1, 2) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                // Ajoute une colonne start_time de type INTEGER
-                database.execSQL("ALTER TABLE performance ADD COLUMN start_time INTEGER NOT NULL DEFAULT 0")
             }
         }
     }
